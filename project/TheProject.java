de78f4e320273d124bbd53f03793b51bca86b48d
fitzylala <skye.fitzpatrick100@gmail.com>
2024-10-22 15:36:43 +0100

    Renamed PCI function and added commented code demonstrating how to call it.

--- project/TheProject.java
+++ project/TheProject.java
@@ -97,7 +97,7 @@ public class TheProject {
         }
     }
 
-    public static List<String> nameVendorDevice (String vendorID, String deviceID) throws Exception {
+    public static List<String> pciNamer (String vendorID, String deviceID) throws Exception {
         /*
         This function takes two inputs for vendor ID and device ID, then returns a string list containing the actual data,
         in the form of ["Vendor Name", "Device Name"]
@@ -105,8 +105,15 @@ public class TheProject {
         Created by Skye Fitzpatrick
         */
 
+        /* TO CALL pciNamer
+        // numbers to be replaced with output from Mark's pciInfo code
+        String vendorName = TheProject.pciNamer("8086", "0180").get(0);
+        String deviceName = TheProject.pciNamer("8086", "0180").get(1);
+        System.out.printf("Vendor: %s%nDevice: %s", vendorName, deviceName);
+        */
+
         // Read in data from the CSV file using Scanner
-        Scanner readCsv = new Scanner(new File("C:\\Users\\skyef\\Documents\\CS4421 - Problem Solving With Computers\\PCI-Lookup-finalfinal.csv"));
+        Scanner readCsv = new Scanner(new File(".\\project\\PCI-Lookup.csv"));
 
         List<List> pciData = new ArrayList(); // Initialise a 2-dimensional array for database rows and columns
         /* We need to create a 2-dimensional array because of the way the csv file is formatted.
