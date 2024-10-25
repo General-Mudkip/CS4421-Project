56dbc4f68bb8462ff8362301f91318792b7447ba
fitzylala <skye.fitzpatrick100@gmail.com>
2024-10-25 14:47:50 +0100

    Created functions for new Disk and Mem info in FetchMarkInfo.
    Also commented out graph code for now (sorry Bence)

--- project/FetchData.java
+++ project/FetchData.java
@@ -1,60 +1,60 @@
 import java.awt.*;
 
 public class FetchData {
-    public static void subMenu(String mainChoice, int subChoice) {
+    public static void subMenu(String mainChoice, int subChoice) throws Exception {
         System.out.println(mainChoice + subChoice);
         cpuInfo CpuInfo = new cpuInfo();
         diskInfo DiskInfo = new diskInfo();
         pciInfo PciInfo = new pciInfo();
 
 
-        if (mainChoice.equals("Cpu_Info1")){
-            if (subChoice == 1){
+        if (mainChoice.equals("Cpu_Info1")) {
+            if (subChoice == 1) {
                 System.out.print(CpuInfo.getModel());
             } else if (subChoice == 2) {
                 System.out.print(CpuInfo.getIdleTime(1));
-            } else if (subChoice == 3){
-                TheProject.menu();
+            } else if (subChoice == 3) {
+                CS4421GroupProject.menu();
 
             }
         }
-        if (mainChoice.equals("Cpu_Info2")){
-            if (subChoice == 1){
+        if (mainChoice.equals("Cpu_Info2")) {
+            if (subChoice == 1) {
                 DiskInfo.diskCount();
             } else if (subChoice == 2) {
-                DiskInfo.getName();
+                DiskInfo.getName(0);
             } else if (subChoice == 3) {
-                TheProject.menu();
+                CS4421GroupProject.menu();
 
             }
         }
-        if (mainChoice.equals("Cpu_Info3")){
-            if (subChoice == 1){
+        if (mainChoice.equals("Cpu_Info3")) {
+            if (subChoice == 1) {
                 System.out.print(PciInfo.deviceCount(1));
-            } else if (subChoice ==2) {
+            } else if (subChoice == 2) {
                 PciInfo.busCount();
-            } else if (subChoice ==3) {
-                TheProject.menu();
+            } else if (subChoice == 3) {
+                CS4421GroupProject.menu();
 
 
             }
         }
-        if (mainChoice.equals("Cpu_Info4")){
-            if (subChoice == 1){
+        if (mainChoice.equals("Cpu_Info4")) {
+            if (subChoice == 1) {
                 CpuInfo.coresPerSocket();
             } else if (subChoice == 2) {
                 CpuInfo.socketCount();
             } else if (subChoice == 3) {
-                TheProject.menu();
+                CS4421GroupProject.menu();
 
             }
 
         }
-        if (mainChoice. equals("Cpu_Info5")){
-            if (subChoice == 1){
+        if (mainChoice.equals("Cpu_Info5")) {
+            if (subChoice == 1) {
                 CpuInfo.l1dCacheSize();
             }
-            if (subChoice == 2){
+            if (subChoice == 2) {
                 CpuInfo.l1iCacheSize();
             }
         }
